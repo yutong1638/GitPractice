fa57be86ce12ac234c52180493c21ca2df07d544
yutong1638 <nicolina9510@gmail.com>
2024-07-16 12:19:31 +0800

    staging area practice

--- gitpractice.js
+++ gitpractice.js
@@ -1,3 +1,5 @@
 document.write("Hello World!");
 alert("123");
 console.log("I add this line in VS code");
+console.log("this line add to show the staging area");
+console.log("this line add to show direct commit(skip the staging area)");
